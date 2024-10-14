a288b056cc1397aa6e4b47ab156bc4a2cd2447c0
fernando-ruiz-rico <48460570+fernando-ruiz-rico@users.noreply.github.com>
2024-10-14 20:38:06 +0200

    Versión inicial de Empresa

---
+++ Ejercicios/Programación Orientada a Objetos/Empresa.java
@@ -0,0 +1,65 @@
+public class Empresa {
+    private String nombre;
+    private Persona[] empleados;
+    private int numEmpleados;
+    
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        this.empleados = new Persona[100];
+        this.numEmpleados = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Persona[] getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(Persona[] empleados) {
+        this.empleados = empleados;
+    }
+
+    public int getNumEmpleados() {
+        return numEmpleados;
+    }
+
+    public void setNumEmpleados(int numEmpleados) {
+        this.numEmpleados = numEmpleados;
+    }
+
+    public void contrataEmpleado(Persona empleado) {
+        if (numEmpleados < empleados.length) {
+            empleados[numEmpleados] = empleado;
+            numEmpleados++;
+        }
+    }
+
+    public void mostrarEmpleados() {
+        for(int i=0; i < numEmpleados; i++) {
+            System.out.println("Nombre: " + empleados[i].getNombre() + " - Edad: " + empleados[i].getEdad());
+        }
+    }
+
+    public void despideEmpleado(int pos) {
+        for(int i=pos; i < numEmpleados - 1; i++) {
+            empleados[i] = empleados[i+1];
+        }
+        numEmpleados--;
+    } 
+
+    public void despideEmpleado(String nombre) {
+        for(int i=0; i < numEmpleados; i++) {
+            if (empleados[i].getNombre().equals(nombre)) {
+                despideEmpleado(i);
+                break;
+            }
+        }
+        numEmpleados--;
+    } 
+}
