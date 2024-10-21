a38bbaef73918596db8a176cf2291fe23f7f04dc
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-21 17:06:54 +0200

    Añadido apartado1 JDBC eoi2

--- Ejercicios/JDBC/eoi2/src/main/java/com/example/App.java
+++ Ejercicios/JDBC/eoi2/src/main/java/com/example/App.java
@@ -1,13 +1,37 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final String db = "jdbc:mysql://localhost:3306/eoi2";
+    private static final String user = "root";
+    private static final String pass = "";
+
+    public static void apartado1() {
+        try (Connection conn = DriverManager.getConnection(db, user, pass);
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery("select * from categorias")) {
+
+            while(rs.next()) {
+                System.out.println(rs.getInt("categoria") + " - " +
+                                   rs.getString("titulo") + " - " +
+                                   rs.getInt("salario") + " euros - " +
+                                   rs.getInt("trienio") + " euros.");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        apartado1();
     }
 }
