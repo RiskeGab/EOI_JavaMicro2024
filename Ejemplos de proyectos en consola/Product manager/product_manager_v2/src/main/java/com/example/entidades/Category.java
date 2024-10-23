31370c08b32047931485150cfaf8b6c9219b0984
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-23 19:01:54 +0200

    Añadidos getters y setters DAO product_manager_v2

--- Ejemplos de proyectos en consola/Product manager/product_manager_v2/src/main/java/com/example/entidades/Category.java
+++ Ejemplos de proyectos en consola/Product manager/product_manager_v2/src/main/java/com/example/entidades/Category.java
@@ -1,5 +1,32 @@
 package com.example.entidades;
 
 public class Category {
+    private int id;
+    private String nombre;
+    
+    public Category(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", nombre=" + nombre + "]";
+    }
 }
