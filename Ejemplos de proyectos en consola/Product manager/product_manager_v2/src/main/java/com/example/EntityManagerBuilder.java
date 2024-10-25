327044c51bbb984a09e4d8cfb1d484da1821c6e0
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-25 16:46:15 +0200

    EntityManagerFactory

---
+++ Ejemplos de proyectos en consola/Product manager/product_manager_v2/src/main/java/com/example/EntityManagerBuilder.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerBuilder {
+	private static EntityManagerFactory emf = null;
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("product_manager_v2");
+		}
+		return emf;
+	}
+
+	public static void closeEntityManagerFactory() {
+		if (emf != null) {
+			emf.close();
+			emf = null;
+		}
+	}
+}
