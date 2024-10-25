343399d3b26798832737557adc007df0ee9c3c70
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-25 17:08:43 +0200

    Definida clave primaria categoría

--- Ejemplos de proyectos en consola/Product manager/product_manager_v2/src/main/java/com/example/entidades/Category.java
+++ Ejemplos de proyectos en consola/Product manager/product_manager_v2/src/main/java/com/example/entidades/Category.java
@@ -1,6 +1,9 @@
 package com.example.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
@@ -13,6 +16,8 @@ import lombok.Data;
 })
 @Data @AllArgsConstructor
 public class Category {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
 
