3b609289aa0818b3363ac01a4bf3055e36ac3b94
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-28 21:14:13 +0100

    Corregidos algunos comentarios

--- Ejemplos de proyectos con Spring/traductor-muy-simple/traductor/src/main/java/com/example/traductor/TraductorApplication.java
+++ Ejemplos de proyectos con Spring/traductor-muy-simple/traductor/src/main/java/com/example/traductor/TraductorApplication.java
@@ -17,26 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class TraductorApplication {
-    // Dos mapas para almacenar las traducciones: de inglés a español y de español a inglés y de inglés a italiano
+    // Tres mapas para almacenar las traducciones: de inglés a español, de español a inglés y de inglés a italiano
     private static Map<String, String> inglesEspanol = new HashMap<>();
     private static Map<String, String> espanolIngles = new HashMap<>();
     private static Map<String, String> inglesItaliano = new HashMap<>();
 
-    // Método para cargar las traducciones desde dos archivos de texto
+    // Método para cargar las traducciones desde tres archivos de texto
     private static void cargarCorpus(String archivoIngles, String archivoEspanol, String archivoItaliano) {
         try {
-            // Lee las líneas de los archivos de texto en inglés y español e italiano
+            // Lee las líneas de los archivos de texto en inglés, español e italiano
             List<String> lineasIngles = Files.readAllLines(Paths.get("traducciones/" + archivoIngles));
             List<String> lineasEspanol = Files.readAllLines(Paths.get("traducciones/" + archivoEspanol));
             List<String> lineasItaliano = Files.readAllLines(Paths.get("traducciones/" + archivoItaliano));
 
-            // Recorre las líneas de ambos archivos y las guarda en los mapas
+            // Recorre las líneas de los archivos y las guarda en los mapas
             for (int i=0; i < lineasIngles.size(); i++) {
                 String fraseIngles = lineasIngles.get(i);  // Frase en inglés
                 String fraseEspanol = lineasEspanol.get(i);  // Frase en español
                 String fraseItaliano = lineasItaliano.get(i);  // Frase en italiano
 
-                // Asocia las frases en ambos mapas para las traducciones en ambas direcciones
+                // Asocia las frases en los mapas para las traducciones en ambas direcciones
                 inglesEspanol.put(fraseIngles, fraseEspanol);  // Traducción inglés -> español
                 espanolIngles.put(fraseEspanol, fraseIngles);  // Traducción español -> inglés
                 inglesItaliano.put(fraseIngles, fraseItaliano);  // Traducción ingles -> italiano
