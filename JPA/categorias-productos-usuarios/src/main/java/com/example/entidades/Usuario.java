f74816e00eb1bd4aaa341f01e833e6cc55d2fae0
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-28 17:47:14 +0100

    Entidad Usuario

---
+++ JPA/categorias-productos-usuarios/src/main/java/com/example/entidades/Usuario.java
@@ -0,0 +1,48 @@
+package com.example.entidades;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name="Usuario.findAll", query="select u from Usuario u")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    private String nombre;
+
+    @ManyToMany(cascade = {
+        CascadeType.PERSIST,
+        CascadeType.MERGE
+    })
+    @JoinTable(
+        name = "productos_favoritos",
+        joinColumns = @JoinColumn(name = "idUsuario"),
+        inverseJoinColumns = @JoinColumn(name = "idProducto")
+    )
+    private Set<Producto> favoritos;
+}
