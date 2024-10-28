b0aa5de20edde1864f14af34ee6556ba84c86ce1
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-28 20:52:25 +0100

    Traducción de inglés -> Español + Italiano

--- Ejemplos de proyectos con Spring/traductor-muy-simple/traductor/src/main/java/com/example/traductor/TraductorApplication.java
+++ Ejemplos de proyectos con Spring/traductor-muy-simple/traductor/src/main/java/com/example/traductor/TraductorApplication.java
@@ -17,25 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class TraductorApplication {
-    // Dos mapas para almacenar las traducciones: de inglés a español y de español a inglés
+    // Dos mapas para almacenar las traducciones: de inglés a español y de español a inglés y de inglés a italiano
     private static Map<String, String> inglesEspanol = new HashMap<>();
     private static Map<String, String> espanolIngles = new HashMap<>();
+    private static Map<String, String> inglesItaliano = new HashMap<>();
 
 	    // Método para cargar las traducciones desde dos archivos de texto
-    private static void cargarCorpus(String archivoIngles, String archivoEspanol) {
+    private static void cargarCorpus(String archivoIngles, String archivoEspanol, String archivoItaliano) {
         try {
-            // Lee las líneas de los archivos de texto en inglés y español
+            // Lee las líneas de los archivos de texto en inglés y español e italiano
             List<String> lineasIngles = Files.readAllLines(Paths.get("traducciones/" + archivoIngles));
             List<String> lineasEspanol = Files.readAllLines(Paths.get("traducciones/" + archivoEspanol));
+            List<String> lineasItaliano = Files.readAllLines(Paths.get("traducciones/" + archivoItaliano));
 
             // Recorre las líneas de ambos archivos y las guarda en los mapas
             for (int i=0; i < lineasIngles.size(); i++) {
                 String fraseIngles = lineasIngles.get(i);  // Frase en inglés
                 String fraseEspanol = lineasEspanol.get(i);  // Frase en español
+                String fraseItaliano = lineasItaliano.get(i);  // Frase en italiano
 
                 // Asocia las frases en ambos mapas para las traducciones en ambas direcciones
                 inglesEspanol.put(fraseIngles, fraseEspanol);  // Traducción inglés -> español
                 espanolIngles.put(fraseEspanol, fraseIngles);  // Traducción español -> inglés
+                inglesItaliano.put(fraseIngles, fraseItaliano);  // Traducción ingles -> italiano
             }
         }
         catch (Exception e) {
@@ -44,14 +48,20 @@ public class TraductorApplication {
     }
 
 	public static void main(String[] args) {
-		cargarCorpus("informatica.en", "informatica.es");
+		cargarCorpus("informatica.en", "informatica.es", "informatica.it");
 		SpringApplication.run(TraductorApplication.class, args);
 	}
 
 	@GetMapping("/traducir")
 	public String traducir(@RequestParam String texto) {
         String traduccion = inglesEspanol.get(texto);
-        if (traduccion == null) traduccion = espanolIngles.get(texto);
+        if (traduccion != null) {
+            traduccion += " / " + inglesItaliano.get(texto);
+        }
+        else {
+            traduccion = espanolIngles.get(texto);
+        }
+
 		return traduccion == null ? "" : traduccion;
 	}
 
