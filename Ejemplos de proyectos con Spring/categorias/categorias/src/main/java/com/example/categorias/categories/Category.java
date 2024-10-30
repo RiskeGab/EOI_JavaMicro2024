3cc2e195a89b463f0136c34ef6a8c315e8db0916
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-30 17:42:47 +0100

    Category.java

---
+++ Ejemplos de proyectos con Spring/categorias/categorias/src/main/java/com/example/categorias/categories/Category.java
@@ -0,0 +1,16 @@
+package com.example.categorias.categories;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    private int id;
+    private String name;
+}
