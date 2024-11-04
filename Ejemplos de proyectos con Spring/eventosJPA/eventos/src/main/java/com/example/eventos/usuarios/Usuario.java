f8ad64e02dabdc55a5eb54b3ea55cdab8481f9d1
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-04 17:24:19 +0100

    Tabla Usuario

---
+++ Ejemplos de proyectos con Spring/eventosJPA/eventos/src/main/java/com/example/eventos/usuarios/Usuario.java
@@ -0,0 +1,35 @@
+package com.example.eventos.usuarios;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String correo;
+
+    /*@ManyToMany(cascade = {
+        CascadeType.PERSIST,
+        CascadeType.MERGE
+    })
+    @JoinTable(
+        name = "usuario_asiste_evento",
+        joinColumns = @JoinColumn(name = "usuario"),
+        inverseJoinColumns = @JoinColumn(name = "evento"))
+    private List<Evento> eventos; */
+}
