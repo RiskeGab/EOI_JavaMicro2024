fde51e5b5643d30a730a60e173ae88697c8028c6
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-08 18:35:00 +0100

    Corregido error en el controller

--- Ejemplos de proyectos con Spring/equiposJPA/equipos/src/main/java/com/example/equipos/equipos/EquiposController.java
+++ Ejemplos de proyectos con Spring/equiposJPA/equipos/src/main/java/com/example/equipos/equipos/EquiposController.java
@@ -1,12 +1,19 @@
 package com.example.equipos.equipos;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.equipos.equipos.dto.EquipoDTO;
+import com.example.equipos.equipos.dto.RespuestaEquipoDTO;
 import com.example.equipos.equipos.dto.RespuestaEquiposDTO;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @CrossOrigin("*")
@@ -21,4 +28,9 @@ public class EquiposController {
         return new RespuestaEquiposDTO(equiposService.getAll());
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RespuestaEquipoDTO postMethodName(@RequestBody @Valid EquipoDTO e) {        
+        return new RespuestaEquipoDTO(equiposService.insert(e));
+    }
 }
